4b2d69c7daab014fb47a9a8291f096a1a163b982
Mark Porter <mark.porter@highgear.com>
2024-05-30 10:25:11 -0400

    MARK: Show the task on notification click
    Skip the useless dialog

--- app/src/main/java/org/tasks/reminders/NotificationActivity.kt
+++ app/src/main/java/org/tasks/reminders/NotificationActivity.kt
@@ -25,6 +25,11 @@ class NotificationActivity : AppCompatActivity(), NotificationDialog.Notificatio
         super.onCreate(savedInstanceState)
         val intent = intent
         taskId = intent.getLongExtra(EXTRA_TASK_ID, 0L)
+
+        //MARK: Launch the edit action right away and skip the useless dialog
+        this.edit()
+        return
+        
         val fragmentManager = supportFragmentManager
         var fragment = fragmentManager.findFragmentByTag(FRAG_TAG_NOTIFICATION_FRAGMENT) as NotificationDialog?
         if (fragment == null) {
